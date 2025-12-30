fcc5ecb6c4e6f21ec6276b2361dace4bb323d0c1
skylexwot <daniil.fominykh23@gmail.com>
2025-12-30 17:41:50 +0100

    Dark-Theme-Redesign: Neues UI, Sidebar & ValueConverter
    Das gesamte UI wurde auf ein modernes, dunkles Farbschema umgestellt. Die Sidebar wurde neu gestaltet und strukturiert, inklusive neuer Menüführung und Sektionen. QuotationControl und QuotedAssetsControl erhielten ein neues Layout mit abgesetzten Karten, Chips und angepassten DataGrid-Styles. Zwei neue ValueConverter (ObjectNullToVisibility, ObjectNotNullToVisibility) steuern die Sichtbarkeit von UI-Elementen. Fehler- und Statusmeldungen erscheinen jetzt als farblich hervorgehobene Info-Boxen. Das Design ist nun konsistent, übersichtlicher und entspricht dem DIAdata-Branding.

---
+++ DIAdataDesktop/Converters/ObjectNotNullToVisibility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DIAdataDesktop.Converters
+{
+    public sealed class ObjectNotNullToVisibility : IValueConverter
+    {
+        public bool Collapse { get; set; } = true;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var visible = value != null;
+            if (visible) return Visibility.Visible;
+            return Collapse ? Visibility.Collapsed : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => Binding.DoNothing;
+    }
+}
---
+++ DIAdataDesktop/Converters/ObjectNullToVisibility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DIAdataDesktop.Converters
+{
+    public class ObjectNullToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isNull = value is null;
+            if (parameter is string p && p.Equals("invert", StringComparison.OrdinalIgnoreCase))
+                isNull = !isNull;
+            return isNull ? Visibility.Collapsed : Visibility.Visible;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => Binding.DoNothing;
+    }
+}
